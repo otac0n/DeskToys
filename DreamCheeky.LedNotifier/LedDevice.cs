f85546f87a1eaeed665048ce2af9ccd84acef7bc
John Gietzen <otac0n@gietzen.us>
2013-08-02 08:22:52 -0700

    Added the LedNotifier project with the LedDevice class.

---
+++ DreamCheeky.LedNotifier/LedDevice.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using HidLibrary;
+
+namespace DreamCheeky.LedNotifier
+{
+    public class LedDevice : IDisposable
+    {
+        public static readonly int MailboxFriendsAlertProductId = 0x0A;
+        public static readonly int WebMailNotifierProductId = 0x04;
+
+        private static readonly byte[] colorData = { 0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x05 };
+        private static readonly byte[][] initData =
+        {
+            new byte[] { 0, 0x1F, 0x02, 0x00, 0x5F, 0x00, 0x00, 0x1F, 0x03 },
+            new byte[] { 0, 0x00, 0x02, 0x00, 0x5F, 0x00, 0x00, 0x1F, 0x04 },
+            new byte[] { 0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 },
+        };
+        private readonly HidDevice device;
+        private bool initialized;
+        private byte redRange = 60;
+        private byte greenRange = 60;
+        private byte blueRange = 60;
+
+        public LedDevice(int deviceIndex = 0, int productId = 0x0A, int vendorId = 0x1D34)
+        {
+            this.device = HidDevices.Enumerate(vendorId, productId).Skip(deviceIndex).Take(1).SingleOrDefault();
+
+            if (this.device == null)
+            {
+                throw new ArgumentException("The given combination of vendorId, productId, and deviceIndex was invalid: no device found.");
+            }
+        }
+
+        public byte RedRange
+        {
+            get { return this.redRange; }
+            set { this.redRange = value; }
+        }
+
+        public byte GreenRange
+        {
+            get { return this.greenRange; }
+            set { this.greenRange = value; }
+        }
+
+        public byte BlueRange
+        {
+            get { return this.blueRange; }
+            set { this.blueRange = value; }
+        }
+
+        public static int GetDeviceCount(int vendorId = 7476, int productId = 4)
+        {
+            return HidDevices.Enumerate(vendorId, productId).Count();
+        }
+
+        public Task SetColor(Color color)
+        {
+            var data = new byte[colorData.Length];
+            Array.Copy(colorData, data, colorData.Length);
+            data[1] = (byte)Math.Floor(this.redRange * (color.R / 255.0));
+            data[2] = (byte)Math.Floor(this.greenRange * (color.G / 255.0));
+            data[3] = (byte)Math.Floor(this.blueRange * (color.B / 255.0));
+
+            return Write(data);
+        }
+
+        private bool Initialize()
+        {
+            bool success = true;
+            for (int i = 0; i < initData.Length; i++)
+            {
+                success = success && this.WriteSync(initData[i]);
+            }
+            this.initialized = true;
+            return success;
+        }
+
+        private Task<bool> Write(byte[] data)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                bool success = this.initialized
+                    ? true
+                    : this.Initialize();
+
+                return success && this.WriteSync(data);
+            });
+        }
+
+        private bool WriteSync(byte[] data)
+        {
+            lock (this.device)
+            {
+                bool result = false;
+                this.device.Write(data, r =>
+                {
+                    lock (this.device)
+                    {
+                        result = r;
+                        Monitor.Pulse(this.device);
+                    }
+                });
+                Monitor.Wait(this.device);
+                return result;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.device.Dispose();
+        }
+    }
+}
---
+++ DreamCheeky.LedNotifier/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DreamCheeky.LedNotifier")]
+[assembly: AssemblyProduct("DreamCheeky.LedNotifier")]
+[assembly: AssemblyCopyright("Copyright © John Gietzen 2013")]
+[assembly: Guid("0ec20ea3-af5c-40fb-b773-5d3d5f33018f")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
