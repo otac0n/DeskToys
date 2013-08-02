e90e4a95598195f870204367614c7080aa20095d
John Gietzen <otac0n@gietzen.us>
2013-08-02 08:30:04 -0700

    Updated assembly info for NuGet release.

--- DreamCheeky.LedNotifier/Properties/AssemblyInfo.cs
+++ DreamCheeky.LedNotifier/Properties/AssemblyInfo.cs
@@ -1,9 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("DreamCheeky.LedNotifier")]
-[assembly: AssemblyProduct("DreamCheeky.LedNotifier")]
+[assembly: AssemblyCompany("John Gietzen")]
 [assembly: AssemblyCopyright("Copyright © John Gietzen 2013")]
+[assembly: AssemblyDescription(".NET Driver for Dream Cheeky LED devices.")]
+[assembly: AssemblyProduct("DreamCheeky.LedNotifier")]
+[assembly: AssemblyTitle("DreamCheeky.LedNotifier")]
 [assembly: Guid("0ec20ea3-af5c-40fb-b773-5d3d5f33018f")]
-[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
