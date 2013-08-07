bba0cfe55015a648f6772a52e7d0155fa9ba420f
John Gietzen <otac0n@gietzen.us>
2013-08-06 18:36:05 -0700

    Added Added the Jam panic btton project.

---
+++ Jam.PanicButton/ButtonDevice.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using HidLibrary;
+
+namespace Jam.PanicButton
+{
+    public class ButtonDevice : IDisposable
+    {
+        private readonly HidDevice device;
+        private readonly Timer timer;
+
+        public ButtonDevice(int deviceIndex = 0, int productId = 0x202, int vendorId = 0x1130)
+        {
+            this.device = HidDevices.Enumerate(vendorId, productId).Where(d => d.Capabilities.NumberFeatureButtonCaps > 0).Skip(deviceIndex).FirstOrDefault();
+
+            if (this.device == null)
+            {
+                throw new ArgumentException("The given combination of vendorId, productId, and deviceIndex was invalid: no device found.");
+            }
+
+            byte[] dummy;
+            this.device.ReadFeatureData(out dummy, 0);
+
+            this.timer = new Timer(Tick, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(10));
+        }
+
+        public event EventHandler<EventArgs> Click;
+
+        public void Dispose()
+        {
+            this.timer.Dispose();
+        }
+
+        private void Tick(object state)
+        {
+            byte[] data;
+            if (this.device.ReadFeatureData(out data, 0))
+            {
+                for (var i = 0; i < data[1]; i++)
+                {
+                    this.RaiseClick();
+                }
+            }
+        }
+
+        private void RaiseClick()
+        {
+            var handler = this.Click;
+            if (handler != null)
+            {
+                handler(this, new EventArgs());
+            }
+        }
+    }
+}
---
+++ Jam.PanicButton/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("John Gietzen")]
+[assembly: AssemblyCopyright("Copyright © John Gietzen 2013")]
+[assembly: AssemblyDescription(".NET Driver for Jam Button devices.")]
+[assembly: AssemblyProduct("Jam.PanicButton")]
+[assembly: AssemblyTitle("Jam.PanicButton")]
+[assembly: Guid("665f625f-81fc-4b77-b250-6797dc30e556")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
