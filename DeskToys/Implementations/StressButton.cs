43cb2b8ec93c3660c2a20d6ef2631379406fe322
John Gietzen <otac0n@gietzen.us>
2013-08-10 10:31:41 -0700

    Added support for the Dream Cheeky Stress Button.

---
+++ DeskToys/Implementations/StressButton.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using HidLibrary;
+
+namespace DeskToys.Implementations
+{
+    public class StressButton : IButton
+    {
+        private HotKeyForm form;
+
+        private StressButton()
+        {
+            this.form = HotKeyForm.Run(Keys.Shift | Keys.Alt | Keys.P);
+            this.form.Press += (o, e) =>
+            {
+                var handler = this.Press;
+                if (handler != null)
+                {
+                    handler(this, e);
+                }
+            };
+        }
+
+        public event EventHandler<EventArgs> Press;
+
+        public static IEnumerable<Service> Enumerate()
+        {
+            if (HidDevices.Enumerate(0x04F3, 0x04A0).Any())
+            {
+                yield return new Service<StressButton>("Dream Cheeky Stress Button", () => new StressButton());
+            }
+        }
+
+        public void Dispose()
+        {
+            this.form.Dispose();
+        }
+
+        private class HotKeyForm : Form
+        {
+            private const int WM_HOTKEY = 0x312;
+            private static int autoId;
+            private int hotKeyId;
+
+            public HotKeyForm(Keys keys)
+            {
+                this.hotKeyId = AutoId;
+
+                var modifiers = keys & Keys.Modifiers;
+
+                var key = (uint)(keys & ~Keys.Modifiers);
+                var modifier =
+                    (modifiers.HasFlag(Keys.Alt) ? 0x01u : 0) +
+                    (modifiers.HasFlag(Keys.Control) ? 0x02u : 0) +
+                    (modifiers.HasFlag(Keys.Shift) ? 0x04u : 0);
+
+                if (!NativeMethods.RegisterHotKey(this.Handle, this.hotKeyId, modifier, key))
+                {
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+            }
+
+            public event EventHandler<EventArgs> Press;
+
+            private static int AutoId
+            {
+                get { return Interlocked.Increment(ref autoId); }
+            }
+
+            public static HotKeyForm Run(Keys keys)
+            {
+                HotKeyForm form = null;
+                var initialized = new ManualResetEvent(false);
+
+                var thread = new Thread(() =>
+                {
+                    form = new HotKeyForm(keys);
+                    initialized.Set();
+                    Application.Run(form);
+                });
+                thread.IsBackground = true;
+                thread.Start();
+
+                initialized.WaitOne();
+                return form;
+            }
+
+            protected override void SetVisibleCore(bool value)
+            {
+                base.SetVisibleCore(false);
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                if (m.Msg == WM_HOTKEY)
+                {
+                    var handler = this.Press;
+                    if (handler != null)
+                    {
+                        handler(this, new EventArgs());
+                    }
+                }
+
+                base.WndProc(ref m);
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    this.Close();
+                }
+
+                if (this.hotKeyId != 0)
+                {
+                    if (!NativeMethods.UnregisterHotKey(this.Handle, this.hotKeyId))
+                    {
+                        throw new Win32Exception(Marshal.GetLastWin32Error());
+                    }
+
+                    this.hotKeyId = 0;
+                }
+
+                base.Dispose(disposing);
+            }
+        }
+    }
+}
---
+++ DeskToys/NativeMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DeskToys
+{
+    public static class NativeMethods
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+    }
+}
