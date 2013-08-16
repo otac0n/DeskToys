795bb493f53e55aec70922a39087ec85cb913284
John Gietzen <otac0n@gietzen.us>
2013-08-16 08:54:02 -0700

    Incremented the version number.

--- DeskToys/Properties/AssemblyInfo.cs
+++ DeskToys/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("DeskToys")]
 [assembly: AssemblyTitle("DeskToys")]
 [assembly: Guid("43b84eae-36f8-4ac0-80fc-5850101198e1")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
-[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
